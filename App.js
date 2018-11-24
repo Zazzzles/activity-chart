7386f3d83a0dc7f2131f339fe225e565c1c2c039
Pedre <pviljoen@fullfacing.com>
2018-11-24 07:42:41 +0200

    Added custom color for activity alert segments

--- App.js
+++ App.js
@@ -9,127 +9,92 @@ export default class App extends React.Component {
       {
         start: "2018-10-20T08:17:03Z",
         end: "2018-10-20T08:17:20Z",
-        active: false
+        color: "red"
       },
       {
         start: "2018-10-20T08:17:20Z",
         end: "2018-10-20T08:17:21Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-10-20T08:17:21Z",
         end: "2018-10-20T08:17:24Z",
-        active: false
+        color: "purple"
       },
       {
         start: "2018-10-20T08:17:24Z",
         end: "2018-10-20T08:17:26Z",
-        active: true
+        color: "blue"
       },
       {
         start: "2018-10-20T08:17:26Z",
         end: "2018-10-20T08:17:28Z",
-        active: false
+        color: "grey"
       },
       {
         start: "2018-10-20T08:17:03Z",
         end: "2018-10-20T08:17:20Z",
-        active: false
+        color: "red"
       },
       {
         start: "2018-11-02T08:17:20Z",
         end: "2018-11-02T08:17:21Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-11-02T08:17:21Z",
         end: "2018-11-02T08:17:24Z",
-        active: false
+        color: "blue"
       },
       {
         start: "2018-11-02T08:17:24Z",
         end: "2018-11-02T08:17:26Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-11-02T08:17:26Z",
         end: "2018-11-02T08:17:28Z",
-        active: false
-      },
-      {
-        start: "2018-11-02T08:17:28Z",
-        end: "2018-11-02T08:17:31Z",
-        active: true
-      },
-      {
-        start: "2018-11-02T08:17:31Z",
-        end: "2018-11-02T08:17:34Z",
-        active: false
-      },
-      {
-        start: "2018-11-02T08:17:34Z",
-        end: "2018-11-02T08:17:58Z",
-        active: true
-      },
-      {
-        start: "2018-11-02T08:17:58Z",
-        end: "2018-11-02T08:18:05Z",
-        active: false
-      },
-      {
-        start: "2018-11-02T08:18:05Z",
-        end: "2018-11-02T08:18:53Z",
-        active: true
-      },
-      {
-        start: "2018-11-02T08:18:53Z",
-        end: "2018-11-02T08:18:58Z",
-        active: false
-      },
-      {
-        start: "2018-11-02T08:18:58Z",
-        end: "2018-11-02T08:18:59Z",
-        active: true
+        color: "purple"
       },
       {
         start: "2018-11-02T08:18:59Z",
         end: "2018-11-02T08:19:17Z",
-        active: false
+        color: "blue"
       },
       {
         start: "2018-11-02T08:19:17Z",
         end: "2018-11-02T08:19:36Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-11-02T08:19:36Z",
         end: "2018-11-02T08:19:57Z",
-        active: false
+        color: "blue"
       },
       {
         start: "2018-11-06T08:18:59Z",
         end: "2018-11-06T08:19:17Z",
-        active: false
+        color: "red"
       },
       {
         start: "2018-11-06T08:19:17Z",
         end: "2018-11-06T08:19:36Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-11-06T08:19:36Z",
         end: "2018-11-06T08:19:57Z",
-        active: false
+        color: "blue"
       },
       {
         start: "2018-11-06T08:18:58Z",
         end: "2018-11-06T08:18:59Z",
-        active: true
+        color: "grey"
       },
       {
         start: "2018-11-06T08:18:59Z",
         end: "2018-11-06T08:19:17Z",
-        active: false
+        color: "red"
       }
     ];
 
@@ -140,8 +105,6 @@ export default class App extends React.Component {
           data={data}
           timeFrame={'1month'}
           barWidth={300}
-          activeColor={"red"}
-          inactiveColor={"grey"}
           barStyle={{ height: 50 }}
           placeholderStyle={{ backgroundColor: "grey" }}
           placeholderText={"No timestamps"}
